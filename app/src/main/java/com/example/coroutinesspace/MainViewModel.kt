94673be7b0dfb02d525249d32eb9de6fe652526d
Bakar.Zhghenti <bakar.zhghenti.1@btu.edu.ge>
2022-02-25 14:28:44 +0400

    second work

--- app/src/main/java/com/example/coroutinesspace/MainViewModel.kt
+++ app/src/main/java/com/example/coroutinesspace/MainViewModel.kt
@@ -2,57 +2,54 @@ package com.example.coroutinesspace
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.Deferred
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.async
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 
-class MainViewModel:ViewModel() {
+class MainViewModel : ViewModel() {
 
 
+    suspend fun calculateOdds(message: String): String {
 
-     suspend fun calculateOdds(message:String): String {
+        var tempBoolean: Boolean
 
-         var tempBoolean: Boolean
-
-         if (message.length<6){
-             jobLessThanSix(message)
-             tempBoolean=true
+        if (message.length < 6) {
+            jobLessThanSix(message)
+            tempBoolean = true
 
 
-         }else{
-             jobMoreThanSix(message)
-             tempBoolean=false
+        } else {
+            jobMoreThanSix(message)
+            tempBoolean = false
 
-         }
+        }
 
-         return if(tempBoolean){
-             "Less than six"
-         }else{
-             "More than six"
-         }
-
+        return if (tempBoolean) {
+            "Less than six"
+        } else {
+            "More than six"
+        }
 
 
     }
 
     private suspend fun jobLessThanSix(string: String): Boolean {
-        val job= viewModelScope.async {
+        val job = viewModelScope.launch(start = CoroutineStart.LAZY) {
+            delay(100)
             Regex(DIGITS).containsMatchIn(string)
 
         }
 
-        return job.await()
+        return job.start()
     }
 
-    private suspend fun jobMoreThanSix(string: String) :String{
-        val job= viewModelScope.async {
+    private suspend fun jobMoreThanSix(string: String): String {
+        val job = viewModelScope.async {
+            delay(100)
             string.uppercase()
         }
         return job.await()
     }
 
-companion object{
-    val DIGITS="[0-9]"
-}
+    companion object {
+        val DIGITS = "[0-9]"
+    }
 }
